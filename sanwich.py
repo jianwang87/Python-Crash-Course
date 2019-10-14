39f930e9593e792f9e390a3fcc7986241ad51e3b
Jian Wang <56494804+jianwang87@users.noreply.github.com>
2019-10-13 22:45:52 -0500

    Update sanwich.py

--- sanwich.py
+++ sanwich.py
@@ -4,6 +4,7 @@ finished_sanwiches = []
 
 while ordered_sanwiches:
     current_sanwich = ordered_sanwiches.pop()
+    #Use .format will make the code clearer.
     print('I will make you a {} sanwich.'.format(current_sanwich))
     processing_sanwiches.append(current_sanwich)
 
@@ -13,4 +14,5 @@ while 'orange' in processing_sanwiches:
 finished_sanwiches = processing_sanwiches
 
 for finished_sanwich in finished_sanwiches:
+    #I saw .center on another toturial but still don't know what that means.
     print('Your {} sanwich is ready. '.format(finished_sanwich).center(10))
